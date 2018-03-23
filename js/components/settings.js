67f6aa1d384216dede40ffab0cbdaa21a3544f06
Vittorio Togliatti <vienianchetu@gmail.com>
2018-03-23 10:37:18 +0100

    Auto stash before merge of "master" and "origin/master"
    Settings

--- js/components/settings.js
+++ js/components/settings.js
@@ -25,43 +25,84 @@ const SettingsTemplate = {props: [],
         template:`
 
         <div>
-                <div>
-                    <md-button class="md-icon-button">
-                        <md-icon>home</md-icon>
-                    </md-button>
+            <div>
+                 <md-tabs>
+                  <md-tab id="tab-home" md-label="Home">
+                    <div>
+                            <md-list>
+                                <md-list-item md-expand>
+                                    <md-icon>videogame_asset</md-icon>
+                                    <span class="md-list-item-text">Games</span>
 
-                    <md-button class="md-icon-button md-accent">
-                        <md-icon>thumb_up</md-icon>
-                    </md-button>
-                </div>
+                                    <md-list slot="md-expand">
+                                      <md-list-item class="md-inset">Console</md-list-item>
+                                      <md-list-item class="md-inset">PC</md-list-item>
+                                      <md-list-item class="md-inset">Phone</md-list-item>
+                                    </md-list>
+                                  </md-list-item>
+                        </md-list>
+                    </div>
+                </md-tab>
 
-                <div>
-                    <md-radio v-model="radio" :value="false">Boolean</md-radio>
-                    <md-radio v-model="radio" value="my-radio">String</md-radio>
-                    <md-radio v-model="radio">No Value</md-radio>
-                    <md-radio v-model="radio" disabled>Disabled</md-radio>
+                  <md-tab id="tab-pages" md-label="Pages">
+                        <div>
+                            <md-button class="md-icon-button">
+                                <md-icon>home</md-icon>
+                            </md-button>
 
-                    <small>Model value: {{ radio }}</small>
-                </div>
+                            <md-button class="md-icon-button md-accent">
+                                <md-icon>thumb_up</md-icon>
+                            </md-button>
+                        </div>
+                        <div>
+                            <md-radio v-model="radio" :value="false">Boolean</md-radio>
+                            <md-radio v-model="radio" value="my-radio">String</md-radio>
+                            <md-radio v-model="radio">No Value</md-radio>
+                            <md-radio v-model="radio" disabled>Disabled</md-radio>
 
-                <div>
-                    <md-dialog-confirm
-                      :md-active.sync="active"
-                      md-title="Use Google's location service?"
-                      md-content="Let Google help apps determine location. <br> This means sending <strong>anonymous</strong> location data to Google, even when no apps are running."
-                      md-confirm-text="Agree"
-                      md-cancel-text="Disagree"
-                      @md-cancel="onCancel"
-                      @md-confirm="onConfirm" />
+                            <small>Model value: {{ radio }}</small>
+                        </div>
+                    </md-tab>
 
-                    <md-button class="md-primary md-raised" @click="active = true">Confirm</md-button>
-                    <span v-if="value">Value: {{ value }}</span>
-                </div>
+                      <md-tab id="tab-posts" md-label="Posts">
+                            <div>
+                                <md-dialog-confirm
+                                  :md-active.sync="active"
+                                  md-title="Use Google's location service?"
+                                  md-content="Let Google help apps determine location. <br> This means sending <strong>anonymous</strong> location data to Google, even when no apps are running."
+                                  md-confirm-text="Agree"
+                                  md-cancel-text="Disagree"
+                                  @md-cancel="onCancel"
+                                  @md-confirm="onConfirm" />
 
-                <div>
-                    <span><md-chip class="md-primary" v-for="chip in primary" :key="chip">{{ chip }}</md-chip></span>
-                    <span><md-chip class="md-accent" v-for="chip in accent" :key="chip" md-clickable>{{ chip }}</md-chip></span>
-                </div>
+                                <md-button class="md-primary md-raised" @click="active = true">Confirm</md-button>
+                                <span v-if="value">Value: {{ value }}</span>
+                            </div>
+                        </md-tab>
+
+                      <md-tab id="tab-favorites" md-label="Favorites">
+                            <div>
+                                <span><md-chip class="md-primary" v-for="chip in primary" :key="chip">{{ chip }}</md-chip></span>
+                                <span><md-chip class="md-accent" v-for="chip in accent" :key="chip" md-clickable>{{ chip }}</md-chip></span>
+                            </div>
+                        </md-tab>
+
+                </md-tabs>
+            </div>     
+
+		              
+
+
+
+
+
+		     
+
+                
+
+                
+
+                
         </div>
 `
                      };
